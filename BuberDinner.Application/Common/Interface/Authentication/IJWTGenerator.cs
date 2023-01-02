e24d648204a6a066ce999165e949caa24b1b7c9f
Hesham <hesham19954@gmail.com>
2023-01-02 23:57:48 +0200

    Created IjwtGenerator and JWT generator

---
+++ BuberDinner.Application/Common/Interface/Authentication/IJWTGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Application.Common.Interface.Authentication
+{
+    public interface IJWTGenerator
+    {
+        string GenerateToken(Guid guid , string FirstName , string LastName);
+    }
+}
---
+++ BuberDinner.Infrastructure/Authentication/JWTGenerator.cs
@@ -0,0 +1,48 @@
+﻿using BuberDinner.Application.Common.Interface.Authentication;
+using BuberDinner.Infrastructure.Constants;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Infrastructure.Authentication
+{
+    public class JWTGenerator : IJWTGenerator
+    {
+        private readonly JWTSettings _jWTSettings;
+
+        public JWTGenerator(IOptions<JWTSettings> jWtoptions)
+        {
+            _jWTSettings = jWtoptions.Value;
+        }
+        public string GenerateToken(Guid guid, string FirstName, string LastName)
+        {
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_jWTSettings.Secret)),
+                SecurityAlgorithms.HmacSha256
+                );
+            var Claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,guid.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName,FirstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName,LastName),
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+
+            };
+
+            var securityToken = new JwtSecurityToken(
+                issuer:_jWTSettings.Issuer,
+                expires: DateTime.Now.AddMinutes(_jWTSettings.ExpiryMinutes),
+                claims: Claims,
+                signingCredentials:signingCredentials);
+
+           return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
