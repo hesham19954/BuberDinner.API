efc3f73fc27c2a888a591bb5386ad4f318610fdc
Hesham <hesham19954@gmail.com>
2023-01-03 23:54:24 +0200

    Added Buber Exception

---
+++ BuberDinner.Domain/Exceptions/BuberException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuberDinner.Domain.Exceptions
+{
+    public class BuberException:Exception
+    {
+        public string? StatusCode { private set; get; }
+        public new IEnumerable<object>? Data { set; get; }
+
+    }
+}
